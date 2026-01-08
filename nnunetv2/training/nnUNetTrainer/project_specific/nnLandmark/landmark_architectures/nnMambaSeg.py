899728976fafae9072f217f7a313195dcc4c97a3
a332l <alexandra.ertl@dkfz-heidelberg.de>
2026-01-08 17:39:32 +0100

    Try nnMambaNew

--- nnunetv2/training/nnUNetTrainer/project_specific/nnLandmark/landmark_architectures/nnMambaSeg.py
+++ nnunetv2/training/nnUNetTrainer/project_specific/nnLandmark/landmark_architectures/nnMambaSeg.py
@@ -77,7 +77,6 @@ def make_res_layer(inplanes, planes, blocks, stride=1, mamba_layer=None):
 
     return nn.Sequential(*layers)
 
-
 class MambaLayer(nn.Module):
     def __init__(self, dim, d_state=16, d_conv=4, expand=2):
         super().__init__()
@@ -91,44 +90,55 @@ class MambaLayer(nn.Module):
         self.norm = nn.BatchNorm3d(dim) # LayerNorm
         self.relu = nn.ReLU(inplace=True)
         self.mamba = Mamba(
-            d_model=dim,  # Model dimension d_model
-            d_state=d_state,  # SSM state expansion factor
-            d_conv=d_conv,  # Local convolution width
-            expand=expand  # Block expansion factor
-        )
+        d_model=dim, # Model dimension d_model
+        d_state=d_state, # SSM state expansion factor
+        d_conv=d_conv, # Local convolution width
+        expand=expand # Block expansion factor
+    )
+
+    # def forward(self, x):
+    #     B, C = x.shape[:2]
+    #     x = self.nin(x)
+    #     x = self.norm(x)
+    #     x = self.relu(x)
+    #     act_x = x
+    #     assert C == self.dim
+    #     n_tokens = x.shape[2:].numel()
+    #     img_dims = x.shape[2:]
+    #     x_flat = x.reshape(B, C, n_tokens).transpose(-1, -2)
+    #     x_mamba = self.mamba(x_flat)
+    #     out = x_mamba.transpose(-1, -2).reshape(B, C, *img_dims)
+    #     act_x = self.relu3(x)
+    #     out += act_x
+    #     out = self.nin2(out)
+    #     out = self.norm2(out)
+    #     out = self.relu2(out)
+    #     return out
+
+# class MambaLayer(nn.Module):
+#     def __init__(self, dim, d_state=16, d_conv=4, expand=2):
+#         super().__init__()
+#         self.dim = dim
+#         self.norm = nn.LayerNorm(dim)
+#         self.mamba = Mamba(
+#             d_model=dim,  # Model dimension d_model
+#             d_state=d_state,  # SSM state expansion factor
+#             d_conv=d_conv,  # Local convolution width
+#             expand=expand  # Block expansion factor
+#         )
 
     def forward(self, x):
         B, C = x.shape[:2]
-        x = self.nin(x)
-        x = self.norm(x)
-        x = self.relu(x)
-        act_x = x
         assert C == self.dim
         n_tokens = x.shape[2:].numel()
         img_dims = x.shape[2:]
         x_flat = x.reshape(B, C, n_tokens).transpose(-1, -2)
-
-        # x_mamba = self.mamba(x_flat)
-
-        x_flip_l = torch.flip(x_flat, dims=[2])
-        x_flip_c = torch.flip(x_flat, dims=[1])
-        x_flip_lc = torch.flip(x_flat, dims=[1,2])
-        x_ori = self.mamba(x_flat)
-        x_mamba_l = self.mamba(x_flip_l)
-        x_mamba_c = self.mamba(x_flip_c)
-        x_mamba_lc = self.mamba(x_flip_lc)
-        x_ori_l = torch.flip(x_mamba_l, dims=[2])
-        x_ori_c = torch.flip(x_mamba_c, dims=[1])
-        x_ori_lc = torch.flip(x_mamba_lc, dims=[1,2])
-        x_mamba = (x_ori+x_ori_l+x_ori_c+x_ori_lc)/4
-
+        x_norm = self.norm(x_flat)
+        x_mamba = self.mamba(x_norm)
         out = x_mamba.transpose(-1, -2).reshape(B, C, *img_dims)
-        # act_x = self.relu3(x)
-        out += act_x
-        out = self.nin2(out)
-        out = self.norm2(out)
-        out = self.relu2(out)
         return out
+
+
 
 
 class DoubleConv(nn.Module):
@@ -224,8 +234,150 @@ class nnMambaSeg(nn.Module):
         return c8
 
 
+class nnMambaSegSL(nn.Module):
+    def __init__(self, in_ch=1, channels=32, blocks=3, number_classes=6):
+        super(nnMambaSegSL, self).__init__()
+        self.in_conv = DoubleConv(in_ch, channels, stride=2, kernel_size=3)
+        # self.mamba_layer_stem = MambaLayer(channels)
+        self.pooling =  nn.AdaptiveAvgPool3d((1, 1, 1))
+
+        self.att1 = Attentionlayer(channels)
+        self.layer1 = make_res_layer(channels, channels * 2, blocks, stride=2)
+        self.mamba_layer_1 = MambaLayer(channels*2)
+
+        self.att2 = Attentionlayer(channels*2)
+        self.layer2 = make_res_layer(channels * 2, channels * 4, blocks, stride=2)
+        self.mamba_layer_2 = MambaLayer(channels*4)
+
+        self.att3 = Attentionlayer(channels*4)
+        self.layer3 = make_res_layer(channels * 4, channels * 8, blocks, stride=2)
+        self.mamba_layer_3 = MambaLayer(channels*8)
+
+        self.up5 = nn.Upsample(scale_factor=2, mode='trilinear', align_corners=False)
+        self.conv5 = DoubleConv(channels * 12, channels * 4)
+        self.up6 = nn.Upsample(scale_factor=2, mode='trilinear', align_corners=False)
+        self.conv6 = DoubleConv(channels * 6, channels * 2)
+        self.up7 = nn.Upsample(scale_factor=2, mode='trilinear', align_corners=False)
+        self.conv7 = DoubleConv(channels * 3, channels)
+        self.up8 = nn.Upsample(scale_factor=2, mode='trilinear', align_corners=False)
+        self.conv8 = DoubleConv(channels, number_classes)
+
+    def forward(self, x):
+        c1 = self.in_conv(x)
+        scale_f1 = self.att1(self.pooling(c1).reshape(c1.shape[0], c1.shape[1])).reshape(c1.shape[0], c1.shape[1], 1, 1, 1)
+        # c1_s = self.mamba_layer_stem(c1) + c1
+        c2 = self.layer1(c1)
+        # c2_s = self.mamba_layer_1(c2) + c2
+        scale_f2 = self.att2(self.pooling(c2).reshape(c2.shape[0], c2.shape[1])).reshape(c2.shape[0], c2.shape[1], 1, 1, 1)
+
+        c3 = self.layer2(c2)
+        # c3_s = self.mamba_layer_2(c3) + c3
+        scale_f3 = self.att3(self.pooling(c3).reshape(c3.shape[0], c3.shape[1])).reshape(c3.shape[0], c3.shape[1], 1, 1, 1)
+        c4 = self.layer3(c3)
+        # c4_s = self.mamba_layer_3(c4) + c4
+
+        up_5 = self.up5(c4)
+        merge5 = torch.cat([up_5, c3*scale_f3], dim=1)
+        c5 = self.conv5(merge5)
+        up_6 = self.up6(c5)
+        merge6 = torch.cat([up_6, c2*scale_f2], dim=1)
+        c6 = self.conv6(merge6)
+        up_7 = self.up7(c6)
+        merge7 = torch.cat([up_7, c1*scale_f1], dim=1)
+        c7 = self.conv7(merge7)
+        up_8 = self.up8(c7)
+        c8 = self.conv8(up_8)
+        return c8
+
+
+# class nnMambaSeg(nn.Module):
+#     def __init__(self, in_ch=1, channels=32, blocks=3, number_classes=6):
+#         super(nnMambaSeg, self).__init__()
+#         self.in_conv = DoubleConv(in_ch, channels, stride=2, kernel_size=3)
+#         # self.mamba_layer_stem = MambaLayer(channels)
+
+#         self.layer1 = make_res_layer(channels, channels * 2, blocks, stride=2)
+#         self.mamba_layer_1 = MambaLayer(channels*2)
+
+#         self.layer2 = make_res_layer(channels * 2, channels * 4, blocks, stride=2)
+#         self.mamba_layer_2 = MambaLayer(channels*4)
+
+#         self.layer3 = make_res_layer(channels * 4, channels * 8, blocks, stride=2)
+#         self.mamba_layer_3 = MambaLayer(channels*8)
+
+#         self.up5 = nn.Upsample(scale_factor=2, mode='trilinear', align_corners=False)
+#         self.conv5 = DoubleConv(channels * 12, channels * 4)
+#         self.up6 = nn.Upsample(scale_factor=2, mode='trilinear', align_corners=False)
+#         self.conv6 = DoubleConv(channels * 6, channels * 2)
+#         self.up7 = nn.Upsample(scale_factor=2, mode='trilinear', align_corners=False)
+#         self.conv7 = DoubleConv(channels * 3, channels)
+#         self.up8 = nn.Upsample(scale_factor=2, mode='trilinear', align_corners=False)
+#         self.conv8 = DoubleConv(channels, number_classes)
+
+#     def forward(self, x):
+#         c1 = self.in_conv(x)
+#         # c1_s = self.mamba_layer_stem(c1) + c1
+#         c2 = self.layer1(c1)
+#         c2_s = self.mamba_layer_1(c2) + c2
+#         c3 = self.layer2(c2_s)
+#         c3_s = self.mamba_layer_2(c3) + c3
+#         c4 = self.layer3(c3_s)
+#         c4_s = self.mamba_layer_3(c4) + c4
+
+#         up_5 = self.up5(c4_s)
+#         merge5 = torch.cat([up_5, c3_s], dim=1)
+#         c5 = self.conv5(merge5)
+#         up_6 = self.up6(c5)
+#         merge6 = torch.cat([up_6, c2_s], dim=1)
+#         c6 = self.conv6(merge6)
+#         up_7 = self.up7(c6)
+#         merge7 = torch.cat([up_7, c1], dim=1)
+#         c7 = self.conv7(merge7)
+#         up_8 = self.up8(c7)
+#         c8 = self.conv8(up_8)
+#         return c8
+
+
+class nnMambaEncoder(nn.Module):
+    def __init__(self, in_ch=1, channels=32, blocks=3, number_classes=1):
+        super(nnMambaEncoder, self).__init__()
+        self.in_conv = DoubleConv(in_ch, channels, stride=2, kernel_size=3)
+        # self.mamba_layer_stem = MambaLayer(channels)
+
+        self.layer1 = make_res_layer(channels, channels * 2, blocks, stride=2)
+        self.mamba_layer_1 = MambaLayer(channels*2)
+
+        self.layer2 = make_res_layer(channels * 2, channels * 4, blocks, stride=2)
+        self.mamba_layer_2 = MambaLayer(channels*4)
+
+        self.layer3 = make_res_layer(channels * 4, channels * 8, blocks, stride=2)
+        self.mamba_layer_3 = MambaLayer(channels*8)
+
+        self.pooling =  nn.AdaptiveAvgPool3d((1, 1, 1))
+        self.mlp = nn.Sequential(nn.Linear(channels*8, channels), nn.ReLU(), nn.Dropout(0.5), nn.Linear(channels, number_classes))
+
+        # self.sig = nn.Sigmoid()
+
+
+    def forward(self, x):
+        c1 = self.in_conv(x)
+        # c1_s = self.mamba_layer_stem(c1) + c1
+        c2 = self.layer1(c1)
+        # c2_s = self.mamba_layer_1(c2) + c2
+        c3 = self.layer2(c2)
+        c3_s = self.mamba_layer_2(c3) + c3
+        c4 = self.layer3(c3)
+        c4_s = self.mamba_layer_3(c4) + c4
+        c5 = self.pooling(c4).view(c4.shape[0], -1)
+        c5 = self.mlp(c5)
+        # c5 = self.sig(c5)
+        return c5
+
+
 if __name__ == "__main__":
     model = nnMambaSeg().cuda()
-    input = torch.zeros((2, 1, 128, 128, 128)).cuda()
+    # model = nnMambaEncoder().cuda()
+
+    input = torch.zeros((8, 1, 128, 128, 128)).cuda()
     output = model(input)
     print(output.shape)
